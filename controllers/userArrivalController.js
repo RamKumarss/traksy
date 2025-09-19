ce06df85bdef4e6a212276f46d794bf4e8164e5e
ramkumarss <ramssss467@gmail.com>
2025-09-19 13:24:13 +0530

    add distance api

--- controllers/userArrivalController.js
+++ controllers/userArrivalController.js
@@ -1,4 +1,5 @@
-import UserArrival from "../models/UserArrival.js";
+import fetch from 'node-fetch';
+import UserArrival from '../models/UserArrival.js';
 
 // Save or Update User Arrival Data
 export const saveUserArrivalData = async (req, res) => {
@@ -12,7 +13,7 @@ export const saveUserArrivalData = async (req, res) => {
       { new: true, upsert: true } // upsert = insert if not found
     );
 
-    res.json({ message: "Data saved/updated!", data: updated });
+    res.json({ message: 'Data saved/updated!', data: updated });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -25,5 +26,30 @@ export const getAllUsersArrivalData = async (req, res) => {
     res.json(allData);
   } catch (err) {
     res.status(500).json({ error: err.message });
+  }
+};
+//Get User Distance Data
+export const getDistance = async (req, res) => {
+  const { lat, lng } = req.query;
+
+  if (!lat || !lng) {
+    return res
+      .status(400)
+      .json({ error: 'Latitude and longitude are required' });
+  }
+
+  try {
+    const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+    const destination = '28.1472,77.3342'; // Fixed destination
+
+    const googleUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${lat},${lng}&destinations=${destination}&mode=driving&departure_time=now&traffic_model=best_guess&key=${GOOGLE_API_KEY}`;
+
+    const googleRes = await fetch(googleUrl);
+    const data = await googleRes.json();
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to fetch distance data' });
   }
 };
--- routes/userArrivalRoutes.js
+++ routes/userArrivalRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllUsersArrivalData, saveUserArrivalData } from "../controllers/userArrivalController";
+import { getAllUsersArrivalData, getDistance, saveUserArrivalData } from "../controllers/userArrivalController";
 
 const router = express.Router();
 
 // Routes
 router.post("/updateArrivalListData", saveUserArrivalData);
 router.get("/getArrivalListData", getAllUsersArrivalData);
+router.get("/distance", getDistance);
 
 export default router;
