2155b839a27f3e983f4955b65113008110f6733e
ramkumarss <ramssss467@gmail.com>
2025-09-19 11:03:58 +0530

    change schema

--- models/UserArrival.js
+++ models/UserArrival.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const UserArrivalSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: String,
-  age: Number,
+  duration: string,
+  distance: string,
+
 });
 
 export default mongoose.model("userarrivaldata", UserArrivalSchema);
