5cfb7a69e36100650a827b03cf85eeadac11d49f
ramkumarss <ramssss467@gmail.com>
2025-09-25 17:23:15 +0530

    implement test api

--- routes/userArrivalRoutes.js
+++ routes/userArrivalRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsersArrivalData, getDistance, saveUserArrivalDataApi } from "../controllers/userArrivalController.js";
+import { getAllUsersArrivalData, getDistance, getDistanceTracky, saveUserArrivalDataApi } from "../controllers/userArrivalController.js";
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post("/updateArrivalListData", saveUserArrivalDataApi);
 router.get("/getArrivalListData", getAllUsersArrivalData);
 router.get("/distance", getDistance);
+router.get("/distanceTracky", getDistanceTracky);
+
 
 export default router;
